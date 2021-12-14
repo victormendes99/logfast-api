1af916766c214a4621bc62fcd0e49167d60042c1
victordsmendes <victor.mendes@lactec.org.br>
2021-12-13 23:49:54 -0300

    Alterando tamanho dos campos estoque e custo

--- web/models.py
+++ web/models.py
@@ -5,7 +5,7 @@ class Item(models.Model):
     descricao = models.CharField(max_length=300, null=False, blank=False)
     subInventario = models.CharField(max_length=10, null=False, blank=False)
     endereco = models.CharField(max_length=60, null=False, blank=True)
-    estoque = models.DecimalField(max_digits=6, decimal_places=2)
+    estoque = models.DecimalField(max_digits=7, decimal_places=2)
     udm = models.CharField(max_length=3, null=False, blank=False)
-    custoUnitario = models.DecimalField(max_digits=6, decimal_places=2)
+    custoUnitario = models.DecimalField(max_digits=10, decimal_places=2)
 
