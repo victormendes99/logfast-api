980cd299f875a75c3d04bd8492f5a33a9663c294
victordsmendes <victor.mendes@lactec.org.br>
2021-12-13 23:15:09 -0300

    Alterando tipo do campo estoque

--- web/models.py
+++ web/models.py
@@ -5,7 +5,7 @@ class Item(models.Model):
     descricao = models.CharField(max_length=300, null=False, blank=False)
     subInventario = models.CharField(max_length=10, null=False, blank=False)
     endereco = models.CharField(max_length=60, null=False, blank=True)
-    estoque = models.IntegerField(null=False, blank=False)
+    estoque = models.DecimalField(max_digits=6, decimal_places=2)
     udm = models.CharField(max_length=2, null=False, blank=False)
     custoUnitario = models.DecimalField(max_digits=6, decimal_places=2)
 
