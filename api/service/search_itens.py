065deb3be1b277e68bba3ac5318c4048ec6022ed
victordsmendes <victor.mendes@lactec.org.br>
2021-12-15 01:41:02 -0300

    Alterando search por subinventario

--- api/service/search_itens.py
+++ api/service/search_itens.py
@@ -6,11 +6,11 @@ def list_all_itens():
 
 def search_itens(item, preventiva):
     if preventiva:
-        itens = Item.objects.filter(descricao__icontains=item).filter(idSubInventario__startswith='P').order_by('descricao')
+        itens = Item.objects.filter(idSubInventario__startswith='P', descricao__icontains=item).order_by('descricao')
     else:
-        itens = Item.objects.filter(descricao__icontains=item).filter(idSubInventario__startswith='M').order_by('descricao')    
+        itens = Item.objects.filter(idSubInventario__startswith='M', descricao__icontains=item).order_by('descricao')    
     return itens
 
 def get_item(item):
-    item = Item.objects.get(id=item)
+    item = Item.objects.get(idSubInventario=item)
     return item
